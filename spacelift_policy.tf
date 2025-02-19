beab65a5051f3e2d8a866826a1d7f35e2951ceb9
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-19 17:56:11 +0530

    Update spacelift_policy.tf

--- spacelift_policy.tf
+++ spacelift_policy.tf
@@ -1,13 +1,35 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.20.0"
+    }
+  }
+}
+
+provider "spacelift" {
+  api_token = var.spacelift_api_token
+}
+
+variable "spacelift_api_token" {
+  description = "API token for Spacelift"
+  type        = string
+  sensitive   = true
+}
+
+# Create a security policy that requires security team approval for changes to aws_iam_policy
 resource "spacelift_policy" "security_approval" {
   name        = "Require Security Approval for IAM Policy Changes"
   type        = "PLAN"
-  body        = file("spacelift-policies/require_security_approval.rego")
+  body        = file("require_security_approval.rego")
   description = "Ensures that security team must approve any aws_iam_policy changes."
 }
 
+# Data source to fetch all stacks from Spacelift
+data "spacelift_stacks" "all_stacks" {}
+
+# Attach the security approval policy to every stack
 resource "spacelift_policy_attachment" "attach_to_all_stacks" {
   policy_id = spacelift_policy.security_approval.id
   stack_ids = [for stack in data.spacelift_stacks.all_stacks.stacks : stack.id]
 }
-
-data "spacelift_stacks" "all_stacks" {}
